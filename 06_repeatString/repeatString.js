f0778554ff7ae0246fe6e34d1864b7cfa428ffff
Jeff <197541511+jliang65@users.noreply.github.com>
2025-05-25 13:16:14 -0500

    Completed exercise 6

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, n) {
+    if(n < 0){
+        return 'ERROR'
+    }
+    target = "";
+    for(let i = 0; i < n; i++){
+        target = target.concat(str);
+    }
+    return target;
 };
 
 // Do not edit below this line
