97d3f2b3b3dcd65e3f31df26a1d6794ad1b075e3
Jeff <197541511+jliang65@users.noreply.github.com>
2025-05-25 13:55:19 -0500

    Completed exercise number 8

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...theRemoved) {
+    for(element of theRemoved){
+        while(arr.includes(element)){
+            arr.splice(arr.indexOf(element), 1);
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
