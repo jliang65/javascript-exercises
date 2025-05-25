9f5698b108740a402814cc46eb9a1e33287abd19
Jeff <197541511+jliang65@users.noreply.github.com>
2025-05-25 16:34:48 -0500

    Finished exercise 9

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    // Filter out non-numbers
+    if(!(typeof(a) === "number" && typeof(b) === "number")){
+        return "ERROR";
+    }
 
+    // Filter out negative numbers
+    if(a < 0 || b < 0){
+        return "ERROR";
+    }
+
+    //Filter out non-integers
+    if(Math.floor(a) != a || Math.floor(b) != b){
+        return "ERROR"; 
+    }
+
+    let min = a < b ? a : b;
+    let max = a > b ? a : b;
+    let sum = 0;
+
+    for(let i = min; i <= max; i++){
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
