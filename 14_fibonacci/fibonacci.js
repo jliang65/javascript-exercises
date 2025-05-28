d27034330f041f9a476b26ba260eeec9665a9cdc
Jeff <197541511+jliang65@users.noreply.github.com>
2025-05-28 13:01:34 -0500

    Completed exercise 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    const number = +n;
+    if(number < 0){
+        return "OOPS";
+    }
 
+    if(number === 0){
+        return 0;
+    }
+    if(number === 1 || number === 2){
+        return 1;
+    }else{
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
 };
 
 // Do not edit below this line
