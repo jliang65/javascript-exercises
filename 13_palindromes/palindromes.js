3029147b6c765eecc144329794dbfca5214d6a2a
Jeff <197541511+jliang65@users.noreply.github.com>
2025-05-28 12:47:09 -0500

    Completed exercise 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,24 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let lString = string.toLowerCase();
+    let letters = lString.split("");
 
+    //remove punctuation and spaces
+    for(let i = 0; i < letters.length;){
+        if(letters[i] === "." || letters[i] === "," 
+            || letters[i] === "?" || letters[i] === "!" || letters[i] === " "){
+            letters.splice(i, 1);
+            continue;
+        }
+        i++;
+    }
+
+    for(let i = 0; i < letters.length; i++){
+        if(letters[i] != letters[letters.length - 1 -i]){
+            return false;
+        }
+
+    }
+    return true;
 };
 
 // Do not edit below this line
