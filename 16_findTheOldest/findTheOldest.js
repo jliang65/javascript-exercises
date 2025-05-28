db317be37af9dbe36cf3a32427fc593142fa8d22
Jeff <197541511+jliang65@users.noreply.github.com>
2025-05-28 17:38:05 -0500

    Complete exercise 16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const getAge = function(person){
+    let recentYear;
+    if('yearOfDeath' in person){
+        recentYear = person.yearOfDeath;
+    }else{
+        recentYear = (new Date()).getFullYear();
+    }
+    return recentYear - person.yearOfBirth;
+}
 
+const findTheOldest = function(people) {
+    return people.reduce((acc, person) => 
+        {if(getAge(acc) > getAge(person)){
+            return acc;
+        }else{
+            return person;
+        }}
+        , people[0]);
 };
 
 // Do not edit below this line
